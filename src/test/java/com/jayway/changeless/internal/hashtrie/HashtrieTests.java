87e7daf9c54d79db9db9b5303b2e3efa017e673e
Sune Simonsen <sune.simonsen@jayway.com>
2011-04-06 07:35:21 +0200

    removed classes introduced by cut and paste

--- src/test/java/com/jayway/changeless/internal/hashtrie/HashtrieTests.java
+++ src/test/java/com/jayway/changeless/internal/hashtrie/HashtrieTests.java
@@ -1,14 +1,8 @@
 package com.jayway.changeless.internal.hashtrie;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
 
