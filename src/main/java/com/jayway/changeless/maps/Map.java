e7e1a98ec25991b74b9f3fdda41c76e8807adc03
Sune Simonsen <sune.simonsen@jayway.com>
2011-04-12 21:01:16 +0200

    Added Javadoc for interfaces.

--- src/main/java/com/jayway/changeless/maps/Map.java
+++ src/main/java/com/jayway/changeless/maps/Map.java
@@ -4,11 +4,49 @@ import com.jayway.changeless.optionals.Optional;
 import com.jayway.changeless.sequences.Sequenceable;
 import com.jayway.changeless.tuples.Tuple;
 
-
+/**
+ * A map is an immutable data structure that uniquely maps keys to an associated values.
+ *
+ * @param <K> the type of the keys in the map.
+ * @param <V> the type of the values in the map. 
+ */
 public interface Map<K,V> extends Sequenceable<Tuple<K,V>> {
+	/**
+	 * Returns a new map where the given key is associated to the specified value. 
+	 * @param key the key.
+	 * @param value the value.
+	 * @return a new map where the given key is associated to the specified value.
+	 */
 	Map<K, V> put(K key, V value);
+	
+	/**
+	 * Returns an optional value retrieved by looking up the key in this map. If the 
+	 * key is not present in this map none will be returned.
+	 * @param key the key to lookup.
+	 * @return an optional value retrieved by looking up the key. None is returned 
+	 * is the key was not found. 
+	 */
 	Optional<V> get(K key);
+	
+	/**
+	 * Returns the value retrieved by looking up the key in this map. If the 
+	 * key is not present in this map the given default value will be returned. 
+	 * @param key the key to lookup.
+	 * @param defaultValue the default value that will be returned if the key is not found.
+	 * @return the value retrieved by looking up the key in this map. If the 
+	 * key is not present in this map the given default value will be returned.
+	 */
 	V get(K key, V defaultValue);
+	
+	/**
+	 * Returns the number of keys in this map. 
+	 * @return the number of keys in this map.
+	 */
 	int size();
+	
+	/**
+	 * Returns true if this map contains no keys.
+	 * @return true if this map contains no keys; false otherwise.
+	 */
 	boolean isEmpty();
 }
--- src/main/java/com/jayway/changeless/vectors/Vector.java
+++ src/main/java/com/jayway/changeless/vectors/Vector.java
@@ -2,12 +2,53 @@ package com.jayway.changeless.vectors;
 
 import com.jayway.changeless.sequences.Sequenceable;
 
+/**
+ * A vector is a growable array of objects where integer indexes maps to 
+ * elements.
+ * @param <T> the type of the elements of the vector.
+ */
 public interface Vector<T> extends Sequenceable<T> {
+	/**
+	 * Returns the element at the given index in this vector.
+	 * @param index the index of the element to be retrieved.
+	 * @return the element at the given index in this vector.
+	 * @throws IllegalArgumentException if the index is less than zero or 
+	 * larger than or equals to the size of this vector.
+	 */
 	T get(int index);
+	
+	/**
+	 * Returns a new vector where the element at the given index is set to 
+	 * the specified element. 
+	 * @param index the index of the element to be changed.
+	 * @param element the element to be inserted in this vector.
+	 * @return a new vector where the element at the given index is set to 
+	 * the specified element.
+	 * @throws IllegalArgumentException if the index is less than zero or 
+	 * larger than or equals to the size of this vector.
+	 */
 	Vector<T> set(int index, T element);
+	
+	/**
+	 * Returns true if this vector is empty; false otherwise.
+	 * @return true if this vector is empty; false otherwise.
+	 */
 	boolean isEmpty();
+	
+	/**
+	 * Returns the number of elements in this vector.
+	 * @return the number of elements in this vector.
+	 */
 	int size();
+	
+	/**
+	 * Returns a new vector containing the elements of this vector with the 
+	 * given elements appended to the end. 
+	 * @param elements the elements to be added.
+	 * @return this vector with the given elements added to the end.
+	 */
 	Vector<T> add(T... elements);
+	
 	/**
 	 * Returns a new vector containing all the elements of this vector except 
 	 * the n first elements. If n is less than one this vector is returned.
@@ -15,5 +56,11 @@ public interface Vector<T> extends Sequenceable<T> {
 	 * Notice that the returned sequence is evaluated lazily.
 	 */
 	Vector<T> skip(int n);
+	
+	/**
+	 * Returns a new vector with the n first elements of this vector.
+	 * @param n the number of elements to take from this vector.
+	 * @return a new vector with the n first elements of this vector.
+	 */
 	Vector<T> take(int n);
 }
