e284f5a4f11060c65637fe417df8a927c6ea7c44
Sune Simonsen <sune.simonsen@jayway.com>
2011-04-12 21:09:41 +0200

    Added Javadoc to interfaces

--- src/main/java/com/jayway/changeless/predicates/Predicate.java
+++ src/main/java/com/jayway/changeless/predicates/Predicate.java
@@ -1,5 +1,15 @@
 package com.jayway.changeless.predicates;
 
+/**
+ * A predicate function that returns true or false based on it's input parameter.
+ * Predicates should not have side effects.
+ * @param <T> the type of the input parameter.
+ */
 public interface Predicate<T> {
+	/**
+	 * Applies this predicate to the given input.
+	 * @param input the input to this predicate.
+	 * @return true if this predicate applies to the given input; false otherwise.
+	 */
 	boolean apply(T input);
 }
--- src/main/java/com/jayway/changeless/tuples/Tuple.java
+++ src/main/java/com/jayway/changeless/tuples/Tuple.java
@@ -1,6 +1,21 @@
 package com.jayway.changeless.tuples;
 
+/**
+ * A tuple is an ordered list of elements. 
+ * This is the the tuple of two elements.
+ * @param <T1> the type of the first element.
+ * @param <T2> the type of the second element.
+ */
 public interface Tuple<T1, T2> {
+	/**
+	 * Returns the first element of this tuple.
+	 * @return the first element of this tuple.
+	 */
 	public T1 getFirst();
+	
+	/**
+	 * Returns the second element of this tuple.
+	 * @return the second element of this tuple.
+	 */
 	public T2 getSecond();
 }
