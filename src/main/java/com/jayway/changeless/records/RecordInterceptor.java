bf9421cee2396a249739eb18f088e85666ba9d53
Sune Simonsen <sune.simonsen@jayway.com>
2012-01-20 18:01:52 +0100

    fixed bug in hashcode method

--- src/main/java/com/jayway/changeless/records/RecordInterceptor.java
+++ src/main/java/com/jayway/changeless/records/RecordInterceptor.java
@@ -76,7 +76,6 @@ class RecordInceptor<T extends Record> implements InvocationHandler {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((clazz == null) ? 0 : clazz.hashCode());
 		result = prime * result + ((data == null) ? 0 : data.hashCode());
 		return result;
 	}
