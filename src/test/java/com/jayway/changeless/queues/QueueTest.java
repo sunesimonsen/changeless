3cd5ffafa95a04f0703716eeb6bf4ac91d1ad72a
Sune Simonsen <sune.simonsen@jayway.com>
2012-06-27 08:36:47 +0200

    fixed warning

--- src/test/java/com/jayway/changeless/queues/QueueTest.java
+++ src/test/java/com/jayway/changeless/queues/QueueTest.java
@@ -1,7 +1,6 @@
 package com.jayway.changeless.queues;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
